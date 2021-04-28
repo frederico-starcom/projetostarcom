51c9e146767f8522b478c65dc69762a366e3328e
frederico-starcom <frederico.magalhaes@star-latm.net>
2021-04-28 09:45:39 -0300

    Remoção dos projteos web e desktop

--- gerarxml/main.py
+++ gerarxml/main.py
@@ -2,23 +2,23 @@ import xml.etree.cElementTree as et
 
 from datetime import datetime
 
-
 __version__ = '1.1'
 __author__ = 'Frederico Gustavo Magalhães'
 
 
 # Verifica se o tamanho do registro é igual ao tamanho infomado na função
-def ValidaTamanho(valor, tamanho):
-    linhazero = ''
-    
-    if len(valor) < tamanho:
-        novalinha = linhazero + valor
-        while len(novalinha) != tamanho:
-            linhazero += '0'
-            novalinha = linhazero + valor
-        return novalinha
+def ValidaTamanho(value, size):
+    line = ''
+    if len(value) < size:
+        newline = line + value
+        while len(newline) != size:
+            line += '0'
+            newline = line + value
+        return newline
     else:
-        return (valor)
+        return value
+
+
 
 tamdesenho = 11
 tamvalor = 10
@@ -70,30 +70,30 @@ et.SubElement(transp, 'transportadora')
 count_progressivo = 1
 while True:
     count = 0
-    
+
     produto = et.SubElement(doc, 'prod')
 
     count_progressivo_str = str(count_progressivo)
     linha = ValidaTamanho(count_progressivo_str, tamprogres)
     et.SubElement(produto, 'nLin').text = str(linha)
-        
+
     numdesenho = input('Digite o número do desenho. ')
-    
+
     # Valida a qtde de caracteres digitados no campo DESENHO
     desenho = ValidaTamanho(numdesenho, tamdesenho)
 
     et.SubElement(produto, 'cAdicProdClie').text = desenho
     et.SubElement(produto, 'vProd')
-        
+
     ValorTotalDesenho = input('Informe o valor total do desenho. ').replace(',', '').replace('.', '')
 
     # Valida a qtde de caracteres digitados no campo ValorTotalDesenho
     valor = ValidaTamanho(ValorTotalDesenho, tamvalor)
 
     et.SubElement(produto, 'vLiqTot').text = valor
-    
+
     ValorUnitDesenho = input('Informe o valor uniário do desenho. ').replace(',', '').replace('.', '')
-    
+
     # Valida a qtde de caracteres digitados no campo ValorUnitDesenho
     valorunit = ValidaTamanho(ValorUnitDesenho, tamvalor)
 
@@ -101,21 +101,21 @@ while True:
     et.SubElement(produto, 'vUnCom').text = '000000000000000.00'
     et.SubElement(produto, 'iPesoB').text = '000000000000000.400'
     et.SubElement(produto, 'iPesoL').text = '000000000000000.400'
-    
+
     qtde_progressivo = int(input('Informe a quantidade solicitado do desenho. '))
     et.SubElement(produto, 'qPed').text = str(qtde_progressivo)
-    
+
     et.SubElement(produto, 'unidMedProd').text = 'EA'
-                
+
     NFref = et.SubElement(produto, 'NFref')
     infItensComp = et.SubElement(NFref, 'infItensComp')
     et.SubElement(infItensComp, 'cCAdicProdClie').text = desenho
-                
+
     automotivo = et.SubElement(produto, 'automotivo')
     et.SubElement(automotivo, 'tpFornec').text = 'P'
     infCompCarg = et.SubElement(automotivo, 'infCompCarg')
     et.SubElement(infCompCarg, 'dEmbContida')
-                
+
     # Realiza a inclusão de novos progressivos referentes ao desenho cadastrado ao XML Logístico
     while qtde_progressivo > count:
         ReqIntern = et.SubElement(produto, 'ReqIntern')
@@ -129,13 +129,14 @@ while True:
         et.SubElement(ReqIntern, 'qEmbalag').text = '10'
 
         count += 1
-                
+
     infoTemp = et.SubElement(produto, 'infoTemp')
 
     count_progressivo += 1
-    
-    sair = input(f'Deseja continuar cadastrando os DESENHOS para o XML Logístico {nota}? "S" para SIM e "N" para NÃO ').upper()
-                
+
+    sair = input(
+        f'Deseja continuar cadastrando os DESENHOS para o XML Logístico {nota}? "S" para SIM e "N" para NÃO ').upper()
+
     if sair == 'N':
         break
 
