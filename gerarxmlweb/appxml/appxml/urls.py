f149bd6c6ba0112acde98f51babd81eead035c87
= <=>
2021-04-28 22:07:50 -0300

    Criação dos arquivos estáticos

--- gerarxmlweb/appxml/appxml/urls.py
+++ gerarxmlweb/appxml/appxml/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from xmlweb import views
-
+from appxml.xmlweb import views
 
 urlpatterns = [
     path('', views.index, name='index'),
